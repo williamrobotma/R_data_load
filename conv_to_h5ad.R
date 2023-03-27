6745234a3d67340cc3be31e85023f0f73b6a7922
williamrobotma <mawilliam.v20@gmail.com>
2023-03-27 17:53:41 -0400

    Generated script and env

---
+++ conv_to_h5ad.R
@@ -0,0 +1,20 @@
+# renv::init(bare=TRUE)
+# renv::install(c("Seurat", "mojaveazure/seurat-disk", "satijalab/seurat-data"))
+# renv::snapshot()
+
+library(Seurat)
+library(SeuratData)
+library(SeuratDisk)
+
+# Chijimatsu, Ryota. (2022). Integrated Data of Single cell RNA sequencing for
+# Human Pancreatic Adenocarcinoma (Version 1) [Data set]. Zenodo.
+# https://doi.org/10.5281/zenodo.6024273
+
+# download
+url <- "https://zenodo.org/record/6024273/files/pk_all.rds"
+curl::curl_download(url, basename(url))
+
+# convert to h5ad
+pk_all <- readRDS("./pk_all.rds")
+SaveH5Seurat(pk_all, filename = "pk_all.h5Seurat")
+Convert("pk_all.h5Seurat", dest = "h5ad")
